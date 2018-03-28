9a248e0c25b1ee5b718a3d2571300680955d4554
DuffyTJ89 <tomduffy89@gmail.com>
2018-03-28 13:42:43 +0100

    Start over Weather App

--- UWPWeatherApp/UWPWeatherApp/Properties/AssemblyInfo.cs
+++ UWPWeatherApp/UWPWeatherApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WeatherApp")]
+[assembly: AssemblyTitle("UWPWeatherApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WeatherApp")]
+[assembly: AssemblyProduct("UWPWeatherApp")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
